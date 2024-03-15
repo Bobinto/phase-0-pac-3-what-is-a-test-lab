0e82923addab554659554e5869aa065557e07210
bob <Mansalati18@gmail.com>
2024-03-15 09:26:41 +0300

    complete lab

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const height = 74;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+
+
+describe("Height", () => {
+    it("is less than 40 and greater than 0", () => {
+      expect(height).toBeMoreThan(0)
+      expect(height).toBeLessThan(40);
+    });
+  });
